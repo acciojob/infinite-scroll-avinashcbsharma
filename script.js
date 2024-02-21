4a26d239ca9fb0aaebc0efb0c678325a070e8939
avinashcbsharma <156292691+avinashcbsharma@users.noreply.github.com>
2024-02-21 11:28:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
-//your code here!
+ var lst = document.getElementById("infi-list");
+        let nxt =1;        
+        loadmore = () =>{
+            for (let i = 0; i < 20; i++){
+                let txt = document.createTextNode("item " +nxt++);
+                let li = document.createElement('li').innerText=txt;
+                let br = document.createElement('br');
+                lst.appendChild(li);
+                lst.appendChild(br);
+            }           
+        }       
+loadmore();
 
+lst.addEventListener('scroll' ,function(){
+    if(lst.scrollTop + lst.clientHeight >= lst.scrollHeight) {
+		loadmore();
+	}	
+});
